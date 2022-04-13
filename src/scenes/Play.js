9836d2b34072780514f39770275656b9f1ab5af7
CoffeeBreakr <jesapark@ucsc.edu>
2022-04-12 19:08:43 -0700

    Start of Play Scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,7 +3,13 @@ class Play extends Phaser.Scene{
         super("playScene");
     }
 
-    createImageBitmap() {
-        this.add.text(20,20, "Rocket Patrol Play");
+    create() {
+        //UI Background
+        this.add.rectangle(0,borderUISize + borderPadding, game.config.width, borderUISize*2, 0xB7B7A4).setOrigin(0,0);
+        //UI Borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0x6B705C).setOrigin(0,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0x6B705C).setOrigin(0,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0x6B705C).setOrigin(0,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x6B705C).setOrigin(0,0);
     }
 }
